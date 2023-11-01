d2492ca6b03747ba24e01148b15d4902bd2bb52c
luca <luca.heidland@contechnet.de>
2023-11-01 13:47:02 +0100

    Adding of Enums

---
+++ FinanceManagementLifeSaver/Enums/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FinanceManagementLifesaver.Enums
+{
+	public class AccountType
+	{
+		public AccountType()
+		{
+			Type1,
+			Type2,
+			Type3
+		}
+	}
+}
---
+++ FinanceManagementLifeSaver/Enums/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FinanceManagementLifesaver.Enums
+{
+	public class TransactionType
+	{
+		public Enum TransactionType()
+		{
+			Type1,
+			Type2,
+			Type3
+		}
+	}
+}
