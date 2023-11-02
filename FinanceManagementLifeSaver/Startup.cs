04b7b73f2a36c421243c7c12f7ba48fbacecf9fe
luca <luca.heidland@contechnet.de>
2023-11-02 16:05:00 +0100

    AddedMapper

--- FinanceManagementLifeSaver/Startup.cs
+++ FinanceManagementLifeSaver/Startup.cs
@@ -40,6 +40,7 @@ namespace FinanceManagementLifesaver
 
 
             services.AddControllers();
+            services.AddAutoMapper(Type(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
