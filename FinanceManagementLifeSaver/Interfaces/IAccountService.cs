7434a968cdd6d4e3fffdc0aafc13af7315c6522f
luca <luca.heidland@contechnet.de>
2023-11-02 13:07:19 +0100

    Updated Interfaces

--- FinanceManagementLifeSaver/Interfaces/IAccountService.cs
+++ FinanceManagementLifeSaver/Interfaces/IAccountService.cs
@@ -9,6 +9,7 @@ namespace FinanceManagementLifesaver.Interfaces
     public interface IAccountService
     {
         Task<Account> GetAccountById(int accountId);
+        Task<Account> GetAccountsByUserId(int userId);
         void CreateAccount(Account account);
         void UpdateAccount(Account account);
         void DeleteAccount(int accountId);
--- FinanceManagementLifeSaver/Interfaces/ITransactionService.cs
+++ FinanceManagementLifeSaver/Interfaces/ITransactionService.cs
@@ -9,6 +9,7 @@ namespace FinanceManagementLifesaver.Interfaces
     public interface ITransactionService
     {
         Task<Transaction> GetTransactionById(int transactionId);
+        Task<Transaction> GetTransactionByAccountId(int accountId);
         void CreateTransaction(Transaction transaction);
         void UpdateTransaction(Transaction transaction);
         void DeleteTransaction(int transactionId);
