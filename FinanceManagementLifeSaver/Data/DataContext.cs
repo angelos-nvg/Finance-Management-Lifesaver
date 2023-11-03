70dac75b0ba1132ad434d03d9ede21f8d109d45f
luca <luca.heidland@contechnet.de>
2023-11-03 08:35:19 +0100

    Deleted Incoming out of DataContext

--- FinanceManagementLifeSaver/Data/DataContext.cs
+++ FinanceManagementLifeSaver/Data/DataContext.cs
@@ -16,7 +16,6 @@ namespace FinanceManagementLifesaver.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Account> Accounts { get; set; }
-        public DbSet<Income> Incomes { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
     }
 }
