95df515ab06b7a784cc72ada03cb13ba24d3d182
luca <luca.heidland@contechnet.de>
2023-11-07 14:31:00 +0100

    Fixed double id requiring request

--- FinanceManagementLifeSaver/Services/TransactionService.cs
+++ FinanceManagementLifeSaver/Services/TransactionService.cs
@@ -31,15 +31,15 @@ namespace FinanceManagementLifesaver.Services
             return response;
         }
 
-        public async Task<ServiceResponse<TransactionDTO>> GetTransactionById(TransactionIdDTO transactionId)
+        public async Task<ServiceResponse<TransactionDTO>> GetTransactionById(int transactionId)
         {
             ServiceResponse<TransactionDTO> response = new ServiceResponse<TransactionDTO>();
-            Transaction transaction = await _context.Transactions.FirstOrDefaultAsync(t => t.Id == transactionId.Id);
+            Transaction transaction = await _context.Transactions.FirstOrDefaultAsync(t => t.Id == transactionId);
             response.Data = _mapper.Map<Transaction, TransactionDTO>(transaction);
             return response;
         }
 
-        public async Task<ServiceResponse<IEnumerable<Transaction>>> GetTransactionsByAccountId(AccountIdDTO accountId)
+        public async Task<ServiceResponse<IEnumerable<Transaction>>> GetTransactionsByAccountId(TransactionIdDTO accountId)
         {
             ServiceResponse<IEnumerable<Transaction>> response = new ServiceResponse<IEnumerable<Transaction>>();
             List<Transaction> transactions = (List<Transaction>)await _context.Transactions.Where(a => a.Id == accountId.Id).ToListAsync();
