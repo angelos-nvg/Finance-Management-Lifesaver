45468b526a9bdd03f7f1566d6b6e508c6c77a875
luca <luca.heidland@contechnet.de>
2023-11-08 10:04:51 +0100

    Fixed ITransactionService

--- FinanceManagementLifeSaver/Interfaces/ITransactionService.cs
+++ FinanceManagementLifeSaver/Interfaces/ITransactionService.cs
@@ -12,7 +12,7 @@ namespace FinanceManagementLifesaver.Interfaces
     {
         Task<ServiceResponse<TransactionSaveDTO>> CreateTransaction(TransactionSaveDTO transaction);
         Task<ServiceResponse<TransactionDTO>> GetTransactionById(int transactionId);
-        Task<ServiceResponse<IEnumerable<Transaction>>> GetTransactionsByAccountId(AccountIdDTO accountId);
+        Task<ServiceResponse<IEnumerable<Transaction>>> GetTransactionsByAccountId(TransactionIdDTO accountId);
         Task<ServiceResponse<Transaction>> UpdateTransaction(TransactionDTO transaction);
         Task<ServiceResponse<Transaction>> DeleteTransaction(TransactionIdDTO transactionId);
     }
