060f0e3183fb5a584465842054bea42cf4b2918f
Fatih Kaplan <fatih.kaplan@contechnet.de>
2023-11-13 13:28:11 +0100

    models erstellt

---
+++ FinanceManagementLifeSaver/Models/Document.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceManagementLifesaver.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public Transaction TransactionId { get; set; }  
+        public string Path { get; set; }    
+    }
+}
