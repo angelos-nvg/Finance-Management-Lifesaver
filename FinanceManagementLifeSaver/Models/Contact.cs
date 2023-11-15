4fea709e93ac018da5e2bd4fbfe3a083d4003e67
Fatih Kaplan <fatih.kaplan@contechnet.de>
2023-11-15 08:52:39 +0100

    delete contact und dokument

--- FinanceManagementLifeSaver/Models/Contact.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinanceManagementLifesaver.Models
-{
-    public class Contact
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }   
-    }
-}
--- FinanceManagementLifeSaver/Models/Document.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinanceManagementLifesaver.Models
-{
-    public class Document
-    {
-        public int Id { get; set; }
-        public Transaction TransactionId { get; set; }  
-        public string Path { get; set; }    
-    }
-}
