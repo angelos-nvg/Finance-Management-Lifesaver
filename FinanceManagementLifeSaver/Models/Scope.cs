f9b811e1440e94bc1e9aab3818f1c3f791c9729d
Fatih Kaplan <fatih.kaplan@contechnet.de>
2023-11-15 14:41:20 +0100

    Scope erstellt

---
+++ FinanceManagementLifeSaver/Models/Scope.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Policy;
+
+namespace FinanceManagementLifesaver.Models
+{
+    public class Scope
+    {
+        public int Id { get; set; }
+        public Account AccountId { get; set; }
+
+    }
+}
